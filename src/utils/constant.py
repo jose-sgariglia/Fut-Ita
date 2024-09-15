2b7e4c0812f70c7db54d8c3bcbec624a68dad367
José Sgariglia <josesugariria@gmail.com>
2024-09-15 12:02:18 +0200

    Update chart and dataset

--- src/utils/constant.py
+++ src/utils/constant.py
@@ -8,26 +8,26 @@ DIR_CARTESIAN = DIR_CHART + "cartesian/"
 
 # Dati
 
-ITA_STATE = (    
-    "Liguria",
-    "Lombardia",
-    "Toscana",
-    "Veneto",
-    "Piemonte",
-    "Emilia-Romagna",
-    "Friuli-Venezia Giulia",
-    "Trentino Alto Adige / Südtirol",
-    "Valle d'Aosta / Vallée d'Aoste",
-    "Lazio",
-    "Marche",
-    "Abruzzo",
-    "Molise",
-    "Campania",
-    "Puglia",
-    "Basilicata",
-    "Calabria",
-    "Sicilia",
-    "Sardegna",
-    "Umbria",
-    "Italia"
-)
+ITA_STATE = {    
+    "Liguria": "LIG",
+    "Lombardia": "LOM",
+    "Toscana": "TOS",
+    "Veneto": "VEN",
+    "Piemonte": "PIE",
+    "Emilia-Romagna": "E-R",
+    "Friuli-Venezia Giulia": "FVG",
+    "Trentino Alto Adige / Südtirol": "TAA",
+    "Valle d'Aosta / Vallée d'Aoste": "VDA",
+    "Lazio": "LAZ",
+    "Marche": "MAR",
+    "Abruzzo": "ABR",
+    "Molise": "MOL",
+    "Campania": "CAM",
+    "Puglia": "PUG",
+    "Basilicata": "BAS",
+    "Calabria": "CAL",
+    "Sicilia": "SIC",
+    "Sardegna": "SAR",
+    "Umbria": "UMB",
+    "Italia": "ITA"
+}
